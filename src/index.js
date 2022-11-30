0a6dd275f83653194f0395e1ee9a5051a016477f
msurprised <wasurprised@gmail.com>
2022-11-30 13:02:58 +0300

    try setup gh-pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename='/bakery'>
         <App />
       </BrowserRouter>
     </PersistGate>
